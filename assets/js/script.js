bf73377379c645860c90a8368225f7d67a1c2940
Simon Colman <s_colman@hotmail.com>
2019-12-17 12:56:16 +1100

    local storage implemented

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,237 +1,254 @@
 var apiKey = "4915deb164cf151c9e3b329f1c2270a7";
 
 var cityHistory = [];
-$(document).ready(function() {
+$(document).ready(function () {
+
+    displayHistory()
 
     $("#search").on("click", function () {
-    displayWeather();
-    displayForecast();
+        var city = $("#searchTerm").val().trim();
+        displayWeather(city);
+        displayForecast(city);
 
-})
-
-$(document).on('click', '.savedCity', function () {
-    var city = localStorage.getItem("savedCity") || [];
-    console.log(city)
-    displayWeather()
-    displayForecast()
-    console.log("this was clicked")
-    $(this).empty();
-    $(".city").empty();
-    $(".forecast").empty();
-    $(".day1").empty();
-    $(".day2").empty();
-    $(".day3").empty();
-    $(".day4").empty();
-    $(".day5").empty();
-
-})
-
-$("#clear").on("click", function () {
-    localStorage.clear()
-    $(".savedCity").empty();
-    $(".city").empty();
-    $(".forecast").empty();
-    $(".day1").empty();
-    $(".day2").empty();
-    $(".day3").empty();
-    $(".day4").empty();
-    $(".day5").empty();
-})
-
-
-$("#myLocation"). on("click", function (){
-    $(".city").empty();
-    $(".forecast").empty();
-    $(".day1").empty();
-    $(".day2").empty();
-    $(".day3").empty();
-    $(".day4").empty();
-    $(".day5").empty();
-    getLocation()
-})
-
-function displayWeather() {
-    $(".city").empty();
-    $(".forecast").empty();
-    $(".day1").empty();
-    $(".day2").empty();
-    $(".day3").empty();
-    $(".day4").empty();
-    $(".day5").empty();
-
-    var city = $("#searchTerm").val().trim();
-    var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
-
-    // var queryUvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey &lat={lat}&lon={lon}" (response.coord.lon)(response.coord.lat)
-    console.log($(this))
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
     })
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-            // Log the queryURL
-            // console.log(queryURL);
-            // Log the resulting object
-            console.log(response);
-            console.log(queryUrl)
-
-            iconCode = response.weather[0].icon
-            console.log(iconCode)
-            // var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
-            //  console.log(iconUrl);
-            //  console.log($("#cityImage").eq(0).html());
-            // $("#cityImage").attr('src', iconUrl)
 
 
 
-            var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
-             $('.city').append($('<img />').attr('src', iconUrl));
 
-            // $("#cityImage").attr('src', iconUrl)
+    $(document).on('click', '.saved', function () {
+        var city = localStorage.getItem($(this).text())
+        console.log(city)
+        displayWeather(city)
+        // displayForecast(city)
+        console.log("this was clicked")
+        $(this).hide();
+        $(".city").empty();
+        $(".forecast").empty();
+        $(".day1").empty();
+        $(".day2").empty();
+        $(".day3").empty();
+        $(".day4").empty();
+        $(".day5").empty();
 
-            $(".city").append("<h2>" + response.name + " Weather today" + "</h2>");
-            $(".city").append("<p>" + "Wind Speed:" + response.wind.speed + "</p>");
-            $(".city").append("<p>" + "Humidity:" + response.main.humidity + "</p>");
-            $(".city").append("<p>" + "Temperature (c)" + ((response.main.temp) - 273).toFixed(2) + "</p>");
+    })
 
-            $(".savedCity").append("<button>" + response.name + "</button>")
-            $(".savedCity").children().last().addClass(response.name)
+    $("#clear").on("click", function () {
+        localStorage.clear()
+        $(".savedCity").empty();
+        $(".city").empty();
+        $(".forecast").empty();
+        $(".day1").empty();
+        $(".day2").empty();
+        $(".day3").empty();
+        $(".day4").empty();
+        $(".day5").empty();
+    })
 
-            localStorage.setItem("savedCity", city)
 
+    $("#myLocation").on("click", function () {
+        $(".city").empty();
+        $(".forecast").empty();
+        $(".day1").empty();
+        $(".day2").empty();
+        $(".day3").empty();
+        $(".day4").empty();
+        $(".day5").empty();
+        getLocation()
+    })
+
+    function displayWeather(city) {
+        $(".city").empty();
+        $(".forecast").empty();
+        $(".day1").empty();
+        $(".day2").empty();
+        $(".day3").empty();
+        $(".day4").empty();
+        $(".day5").empty();
+
+
+        var queryUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+        // var queryUvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey &lat={lat}&lon={lon}" (response.coord.lon)(response.coord.lat)
+        console.log($(this))
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
         })
-}
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function (response) {
+                // Log the queryURL
+                // console.log(queryURL);
+                // Log the resulting object
+                console.log(response);
+                console.log(queryUrl)
 
-function displayForecast() {
-    $(".city").empty();
-    event.preventDefault();
-    var city = $("#searchTerm").val().trim();
-    var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
-
-    console.log($(this))
-    $.ajax({
-        url: queryUrl,
-        method: "GET",
-    })
-
-        // We store all of the retrieved data inside of an object called "response"
-        .then(function (response) {
-            // Log the queryURL
-            // console.log(queryURL);
-            // Log the resulting object
-            console.log(response);
-            console.log(queryUrl)
-            // console.log(response.list[0].clouds)
-            console.log(response.list[0].main.temp)
-
-            
+                iconCode = response.weather[0].icon
+                console.log(iconCode)
+                // var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+                //  console.log(iconUrl);
+                //  console.log($("#cityImage").eq(0).html());
+                // $("#cityImage").attr('src', iconUrl)
 
 
 
-            $(".day1").append("<h3>" + response.list[0].dt_txt + "<h3>");
-            $(".day1").attr("<img>" + response.list[0].weather[0].icon + ".png" + "<img>")
-            $(".day1").append("<p>" + "Humidity:" + response.list[0].main.humidity + "</p>");
-            $(".day1").append("<p>" + "Temperature (c)" + ((response.list[0].main.temp) - 273).toFixed(2) + "</p>");
+                var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+                $('.city').append($('<img />').attr('src', iconUrl));
 
-            $(".day2").append("<h3>" + response.list[4].dt_txt + "<h3>");
-            $(".day2").append("<p>" + "Humidity:" + response.list[1].main.humidity + "</p>");
-            $(".day2").append("<p>" + "Temperature (c)" + ((response.list[1].main.temp) - 273).toFixed(2) + "</p>");
+                // $("#cityImage").attr('src', iconUrl)
 
-            $(".day3").append("<h3>" + response.list[10].dt_txt + "<h3>");
-            $(".day3").append("<p>" + "Humidity:" + response.list[2].main.humidity + "</p>");
-            $(".day3").append("<p>" + "Temperature (c)" + ((response.list[2].main.temp) - 273).toFixed(2) + "</p>");
+                $(".city").append("<h2>" + response.name + " Weather today" + "</h2>");
+                $(".city").append("<p>" + "Wind Speed:" + response.wind.speed + "</p>");
+                $(".city").append("<p>" + "Humidity:" + response.main.humidity + "</p>");
+                $(".city").append("<p>" + "Temperature (c)" + ((response.main.temp) - 273).toFixed(2) + "</p>");
 
-            $(".day4").append("<h3>" + response.list[15].dt_txt + "<h3>");
-            $(".day4").append("<p>" + "Humidity:" + response.list[3].main.humidity + "</p>");
-            $(".day4").append("<p>" + "Temperature (c)" + ((response.list[3].main.temp) - 273).toFixed(2) + "</p>");
+                $(".savedCity").append("<button>" + response.name + "</button>")
+                $(".savedCity").children().last().addClass("saved")
 
-            $(".day5").append("<h3>" + response.list[20].dt_txt + "<h3>");
-            $(".day5").append("<p>" + "Humidity:" + response.list[4].main.humidity + "</p>");
-            $(".day5").append("<p>" + "Temperature (c)" + ((response.list[4].main.temp) - 273).toFixed(2) + "</p>");
+                localStorage.setItem(city, city)
+
+            })
+    }
+
+    function displayForecast() {
+        $(".city").empty();
+        event.preventDefault();
+        var city = $("#searchTerm").val().trim();
+        var queryUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey;
+
+        console.log($(this))
+        $.ajax({
+            url: queryUrl,
+            method: "GET",
+        })
+
+            // We store all of the retrieved data inside of an object called "response"
+            .then(function (response) {
+                // Log the queryURL
+                // console.log(queryURL);
+                // Log the resulting object
+                console.log(response);
+                console.log(queryUrl)
+                // console.log(response.list[0].clouds)
+                console.log(response.list[0].main.temp)
 
 
 
-            console.log(response.city.coord.lat)
-            console.log(response.city.coord.lon)
-
-            var lat = response.city.coord.lat
-            var lon = response.city.coord.lon
 
 
-            function displayUv() {
-                var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=9edde4d3d50871b7d0d7074d24782ded&lat=" + lat + "&lon=" + lon;
-                $.ajax({
-                    url: uvUrl,
-                    method: "GET",
-                })
-                    .then(function (response) {
-                        console.log(response)
-                        console.log(uvUrl)
-                        $(".city").append("<p>" + "UVindex:" + response.value + "</p>")
-                        // .addClass(".uVindex");
+                $(".day1").append("<h3>" + response.list[0].dt_txt + "<h3>");
+                $(".day1").attr("<img>" + response.list[0].weather[0].icon + ".png" + "<img>")
+                $(".day1").append("<p>" + "Humidity:" + response.list[0].main.humidity + "</p>");
+                $(".day1").append("<p>" + "Temperature (c)" + ((response.list[0].main.temp) - 273).toFixed(2) + "</p>");
+
+                $(".day2").append("<h3>" + response.list[4].dt_txt + "<h3>");
+                $(".day2").append("<p>" + "Humidity:" + response.list[1].main.humidity + "</p>");
+                $(".day2").append("<p>" + "Temperature (c)" + ((response.list[1].main.temp) - 273).toFixed(2) + "</p>");
+
+                $(".day3").append("<h3>" + response.list[10].dt_txt + "<h3>");
+                $(".day3").append("<p>" + "Humidity:" + response.list[2].main.humidity + "</p>");
+                $(".day3").append("<p>" + "Temperature (c)" + ((response.list[2].main.temp) - 273).toFixed(2) + "</p>");
+
+                $(".day4").append("<h3>" + response.list[15].dt_txt + "<h3>");
+                $(".day4").append("<p>" + "Humidity:" + response.list[3].main.humidity + "</p>");
+                $(".day4").append("<p>" + "Temperature (c)" + ((response.list[3].main.temp) - 273).toFixed(2) + "</p>");
+
+                $(".day5").append("<h3>" + response.list[20].dt_txt + "<h3>");
+                $(".day5").append("<p>" + "Humidity:" + response.list[4].main.humidity + "</p>");
+                $(".day5").append("<p>" + "Temperature (c)" + ((response.list[4].main.temp) - 273).toFixed(2) + "</p>");
+
+
+
+                console.log(response.city.coord.lat)
+                console.log(response.city.coord.lon)
+
+                var lat = response.city.coord.lat
+                var lon = response.city.coord.lon
+
+
+                function displayUv() {
+                    var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?appid=9edde4d3d50871b7d0d7074d24782ded&lat=" + lat + "&lon=" + lon;
+                    $.ajax({
+                        url: uvUrl,
+                        method: "GET",
                     })
-            }
+                        .then(function (response) {
+                            console.log(response)
+                            console.log(uvUrl)
+                            $(".city").append("<p>" + "UVindex:" + response.value + "</p>")
+                            // .addClass(".uVindex");
+                        })
+                }
 
-            displayUv()
+                displayUv()
+            })
+    }
+
+
+
+    function getLocation() {
+
+
+
+        navigator.geolocation.getCurrentPosition(function (position) {
+            var lat = position.coords.latitude
+            var lng = position.coords.longitude
+
+            console.log(lat, lng)
+
+            latLonUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + apiKey
+
+            $.ajax({
+                url: latLonUrl,
+                method: "Get",
+            })
+
+
+                .then(function (response) {
+                    // Log the queryURL
+                    // console.log(queryURL);
+                    // Log the resulting object
+                    console.log(response);
+                    console.log(latLonUrl)
+
+                    iconCode = response.weather[0].icon
+                    console.log(iconCode)
+
+
+                    var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
+                    $('.city').append($('<img />').attr('src', iconUrl));
+                    $(".city").append("<h2>" + response.name + " Weather today" + "</h2>");
+                    $(".city").append("<p>" + "Wind Speed:" + response.wind.speed + "</p>");
+                    $(".city").append("<p>" + "Humidity:" + response.main.humidity + "</p>");
+                    $(".city").append("<p>" + "Temperature (c)" + ((response.main.temp) - 273).toFixed(2) + "</p>");
+
+                    $(".savedCity").children().last().addClass(response.name)
+
+
+
+                })
         })
-}
-
-
-
-function getLocation(){
-
-
-  
-    navigator.geolocation.getCurrentPosition(function(position){
-        var lat = position.coords.latitude
-        var lng = position.coords.longitude
-
-        console.log(lat,lng)
-
-       latLonUrl = "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lng + "&appid=" + apiKey   
-       
-       $.ajax({
-           url: latLonUrl,
-           method: "Get",
-       })
-       
-     
-        .then(function (response) {
-            // Log the queryURL
-            // console.log(queryURL);
-            // Log the resulting object
-            console.log(response);
-            console.log(latLonUrl)
-
-            iconCode = response.weather[0].icon
-            console.log(iconCode)
-          
-
-            var iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png"
-            $('.city').append($('<img />').attr('src', iconUrl));
-            $(".city").append("<h2>" + response.name + " Weather today" + "</h2>");
-            $(".city").append("<p>" + "Wind Speed:" + response.wind.speed + "</p>");
-            $(".city").append("<p>" + "Humidity:" + response.main.humidity + "</p>");
-            $(".city").append("<p>" + "Temperature (c)" + ((response.main.temp) - 273).toFixed(2) + "</p>");
-
-            $(".savedCity").children().last().addClass(response.name)
-
-          
-
-       })
-    })
-}
+    }
 
 
 })
 
 
-$(document).ready(function(){
-    $('select').formSelect();
+function displayHistory() {
+    for (i = 0; i < localStorage.length; i++) {
+        $(".savedCity").append("<button>" + localStorage.getItem(localStorage.key(i)) + "</button>")
+        $(".savedCity").children().last().addClass("saved")
+    }
+}
 
-    
-})
 
 
     // wrap up all the ajax method into a function and store that function as the value 
+
+    // make sure display forecast takes argument the same way as display weather
+    // remove the variable from the function and add an argument
+    // as apose to this.hide try to add a check if the if statement for the condition localstorage .get item and search 
+    // for the city and do a check to see localstorage.getitem(city) ==== true else add to local storage 
+    // call the 
+    // in the display history run the display function localstorage 
+    // call the current location 
+    // google how to retrieve the last index in the array 
